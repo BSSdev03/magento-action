3d56a4018e9c3728b6b3876a0df3f51e9c5f8592
bssdev <bssdev03@gmail.com>
2020-04-08 16:03:40 +0700

    Modify script

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -13,7 +13,7 @@ mkdir ~/.ssh/ && echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_
 ssh-add ~/.ssh/id_rsa
 echo "$SSH_CONFIG" > /etc/ssh/ssh_config && chmod 600 /etc/ssh/ssh_config
 
-
+echo "deploy to $HOST_DEPLOY_PATH"
 echo "Move sites updated code to staging site"
-rsync -e "ssh -o StrictHostKeyChecking=no -p 22" -avz ./ $USER_NAMEh@$ADDRESS:$HOST_DEPLOY_PATH
+rsync -e "ssh -o StrictHostKeyChecking=no -p 22" -avz ./ $USER_NAME@$ADDRESS:$HOST_DEPLOY_PATH
 ssh -o StrictHostKeyChecking=no -p 22 $USER_NAME@$ADDRESS 'bash $HOST_DEPLOY_PATH/staging_deployer.sh'
