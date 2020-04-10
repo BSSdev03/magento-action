5c1d4445bea5718b6cd951a48b8c93f311a337fe
BSS DEV <bssdev03@gmail.com>
2020-04-10 14:00:02 +0700

    Update deploy-prod.sh

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -15,5 +15,6 @@ echo "$SSH_CONFIG" > /etc/ssh/ssh_config && chmod 600 /etc/ssh/ssh_config
 
 echo "deploy to $HOST_DEPLOY_PATH"
 echo "Move sites updated code to staging site"
-rsync -e "ssh -o StrictHostKeyChecking=no -p 22" -avz ./ $USERNAME@$ADDRESS:$HOST_DEPLOY_PATH
-ssh -o StrictHostKeyChecking=no -p 22 $USERNAME@$ADDRESS 'bash /home/cloudpanel/htdocs/www.glassesgallery.com/pro_deploy.sh'
+ls -al
+#rsync -e "ssh -o StrictHostKeyChecking=no -p 22" -avz ./ $USERNAME@$ADDRESS:$HOST_DEPLOY_PATH
+#ssh -o StrictHostKeyChecking=no -p 22 $USERNAME@$ADDRESS 'bash /home/cloudpanel/htdocs/www.glassesgallery.com/pro_deploy.sh'
