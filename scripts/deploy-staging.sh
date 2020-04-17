d87220022b6b636f633320931acf273ca5631374
bssdev <bssdev03@gmail.com>
2020-04-17 17:36:45 +0700

    Update script staging - add auth.json

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -13,6 +13,10 @@ mkdir ~/.ssh/ && echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_
 ssh-add ~/.ssh/id_rsa
 echo "$SSH_CONFIG" > /etc/ssh/ssh_config && chmod 600 /etc/ssh/ssh_config
 
+touch auth.json
+
+echo "$COMPOSER_AUTH" >> auth.json && chmod 600 auth.json
+
 composer self-update --stable
 composer update
 composer install
@@ -20,6 +24,7 @@ php bin/magento setup:upgrade
 php bin/magento setup:static-content:deploy en_US fr_Fr zh_Hant_TW en_CA  -f
 
 rm -f app/etc/env.php
+rm -f auth.json
 
 echo "deploy to $HOST_DEPLOY_PATH"
 echo "Move sites updated code to staging site"
