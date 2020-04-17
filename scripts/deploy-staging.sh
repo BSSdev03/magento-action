2252b1b94a7b1c15ffdb23241798676d2d038f2a
bssdev <bssdev03@gmail.com>
2020-04-17 15:57:43 +0700

    Update script for zero downtime

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -13,6 +13,12 @@ mkdir ~/.ssh/ && echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_
 ssh-add ~/.ssh/id_rsa
 echo "$SSH_CONFIG" > /etc/ssh/ssh_config && chmod 600 /etc/ssh/ssh_config
 
+composer update
+composer install
+php /home/cloudpanel/htdocs/www.glassesgallery.com/bin/magento setup:static-content:deploy en_US fr_Fr zh_Hant_TW en_CA  -f
+
+rm -f app/etc/env.php
+
 echo "deploy to $HOST_DEPLOY_PATH"
 echo "Move sites updated code to staging site"
 rsync -e "ssh -o StrictHostKeyChecking=no -p 22" -avz ./ $USERNAME@$ADDRESS:$HOST_DEPLOY_PATH
