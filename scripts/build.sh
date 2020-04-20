ee5ea60c35ee40c4c8f2885f32feb40c6b40a9bd
bssdev <bssdev03@gmail.com>
2020-04-20 13:38:06 +0700

    Update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,7 +16,7 @@ chmod +x bin/magento
 
 mysqladmin -h mysql -u root -pmagento status
 
-bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome"
+bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation
 
 #run necessary magento command
 php bin/magento setup:upgrade
