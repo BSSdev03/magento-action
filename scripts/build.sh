77f06cb91ddae185c47e081de90772bde8098abc
BSS DEV <bssdev03@gmail.com>
2020-04-21 12:32:33 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,7 +25,7 @@ chmod +x bin/magento
 
 
 mkdir samplemage
-composer create-project --repository-url=https://repo.magento.com/ magento/product-enterprise-edition=2.3.2 samplemage
+/usr/local/bin/composer create-project --repository-url=https://repo.magento.com/ magento/product-enterprise-edition=2.3.2 samplemage
 samplemage/bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento2" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation
 mv samplemage/app/etc/env.php app/etc/
 mv samplemage/app/etc/config.php app/etc/
