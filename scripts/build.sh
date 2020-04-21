468d6d1e0b129fd24aa5cc02784c71929578711e
BSS DEV <bssdev03@gmail.com>
2020-04-21 13:09:10 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,23 +16,20 @@ echo "check db magento exist";
 
 mysqladmin -h mysql -u root -pmagento create magento2
 
+
 chmod +x bin/magento
 
-#sample magento
+
+
 mkdir samplemage
-cd samplemage
-
-/usr/local/bin/composer create-project --repository-url=https://repo.magento.com/ magento/product-enterprise-edition . --no-dev --no-progress
-bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento2" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation
-
-cd ../
-
-#actual project
-composer --version
-/usr/local/bin/composer install --no-dev --no-progress
-
+ls -al
+composer create-project --repository-url=https://repo.magento.com/ magento/project-enterprise-edition samplemage
+samplemage/bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento2" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation
 mv samplemage/app/etc/env.php app/etc/
 mv samplemage/app/etc/config.php app/etc/
+
+composer --version
+/usr/local/bin/composer install --no-dev --no-progress
 
 #run necessary magento command
 php bin/magento setup:upgrade
