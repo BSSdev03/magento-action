831a37a5b6c6fbb480d8fa9178f438bac0503bc8
BSS DEV <bssdev03@gmail.com>
2020-04-21 11:38:54 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,15 +16,19 @@ echo "check db magento exist";
 
 mysqladmin -h mysql -u root -pmagento create magento2
 
-bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento2" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation
 
 composer --version
 /usr/local/bin/composer install --no-dev --no-progress
 
 chmod +x bin/magento
 
-rm -f app/etc/env.php
+rm -f ./app/etc/env.php
 
+mkdir samplemage
+composer create-project --repository-url=https://repo.magento.com/ magento/project-enterprise-edition samplemage
+samplemage/bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento2" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation
+
+bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento2" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation
 
 #run necessary magento command
 php bin/magento setup:upgrade
