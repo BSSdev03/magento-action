944ccfe1952c597a3826241b96158f29ee25eeae
BSS DEV <bssdev03@gmail.com>
2020-04-21 11:19:06 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,17 +18,17 @@ mysqladmin -h mysql -u root -pmagento create magento2
 
 
 composer --version
-/usr/local/bin/composer update
 /usr/local/bin/composer install --no-dev --no-progress
 
 chmod +x bin/magento
 
 rm -f app/etc/env.php
 
-bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento2" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" 
+bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento2" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation
+
 
 #run necessary magento command
-#php bin/magento setup:upgrade
+php bin/magento setup:upgrade
 php bin/magento setup:di:compile
 php bin/magento deploy:mode:set --skip-compilation production
 php bin/magento setup:static-content:deploy en_US fr_Fr zh_Hant_TW en_CA  -f
