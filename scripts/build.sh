c90dcf9f96b08767cc419471e17198c2e7b21e72
BSS DEV <bssdev03@gmail.com>
2020-04-27 23:23:26 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,12 +21,12 @@ chmod +x bin/magento
 
 
 
-mkdir samplemage
-ls -al
-composer create-project --repository-url=https://repo.magento.com/ magento/project-enterprise-edition=2.3.2 samplemage
-samplemage/bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento2" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation
-mv samplemage/app/etc/env.php app/etc/
-mv samplemage/app/etc/config.php app/etc/
+#mkdir samplemage
+#ls -al
+#composer create-project --repository-url=https://repo.magento.com/ magento/project-enterprise-edition=2.3.2 samplemage
+bin/magento setup:install --admin-firstname="local" --admin-lastname="local" --admin-email="local@local.com" --admin-user="local" --admin-password="local123" --base-url="http://magento.build/" --backend-frontname="admin" --db-host="mysql" --db-name="magento2" --db-user="root" --db-password="magento" --use-secure=0 --use-rewrites=1 --use-secure-admin=0 --session-save="db" --currency="EUR" --language="en_US" --timezone="Europe/Rome" --cleanup-database --skip-db-validation
+#mv samplemage/app/etc/env.php app/etc/
+#mv samplemage/app/etc/config.php app/etc/
 
 composer --version
 composer global require hirak/prestissimo
@@ -39,10 +39,10 @@ php bin/magento module:disable -f Amazon_Login Amazon_Payment Amazon_Core Magent
 php bin/magento setup:upgrade
 php bin/magento setup:di:compile
 #php bin/magento deploy:mode:set --skip-compilation production
-php bin/magento setup:static-content:deploy en_US fr_CA fr_FR zh_Hant_TW en_CA -f
+php bin/magento setup:static-content:deploy --jobs=5 en_US fr_CA fr_FR zh_Hant_TW en_CA -f
 
 #composer dump-autoload -o
 
 rm app/etc/env.php
 
-rm -rf samplemage
+#rm -rf samplemage
